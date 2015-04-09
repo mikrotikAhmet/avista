e68198cdb18eba3e4d9ed2724d0482f7cec3d61d
root <root@localhost.localdomain>
2015-04-09 21:04:16 +0200

    Db Reset

--- gateway/portal/language/english/mail/forgotten.php
+++ gateway/portal/language/english/mail/forgotten.php
@@ -15,7 +15,7 @@
 
 // Text
 $_['text_subject']  = '%s - New Password';
-$_['text_greeting'] = 'A new password was requested from %s.';
-$_['text_password'] = 'Your new password is:';
+$_['text_greeting'] = 'A new password was requested.';
+$_['text_password'] = 'Your new temporary password is : %s';
 
 //End of file forgotten.php 
