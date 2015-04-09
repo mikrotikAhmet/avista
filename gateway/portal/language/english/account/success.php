6da12d94126297f428ff10d54c44718850be0c4c
root <root@localhost.localdomain>
2015-04-09 18:17:12 +0200

    Register Form

--- gateway/portal/language/english/account/success.php
+++ gateway/portal/language/english/account/success.php
@@ -20,7 +20,7 @@
 $_['heading_title'] = 'Your Account Has Been Created!';
 
 // Text
-$_['text_message']  = '<p>Congratulations! Your new account has been successfully created!</p> <p>You can now a client of %s Web Transaction Platform.</p> <p>If you have ANY questions about the operation of this Web Transaction Platform, please contact our <a href="mailto:support@hifholding.com">Support Team</a>.</p> <p>A confirmation has been sent to the provided e-mail address. If you have not received it within the hour, please <a href="%s">contact us</a>.</p>';
+$_['text_message']  = '<p>Congratulations! Your new account has been successfully created!</p> <p>You are now a client of %s Web Transaction Platform.</p> <p>If you have ANY questions about the operation of this Web Transaction Platform, please contact our <a href="mailto:support@hifholding.com">Support Team</a>.</p> <p>A confirmation has been sent to the provided e-mail address. If you have not received it within the hour, please <a href="mailto:office@hifholding.com">contact us</a>.</p>';
 $_['text_approval'] = '<p>Thank you for registering with %s!</p><p>You will be notified by e-mail once your account has been activated by our underwriting team.</p><p>If you have ANY questions about the operation of Web Transaction Platform, please <a href="%s">contact us</a>.</p>';
 $_['text_account']  = 'Account';
 $_['text_success']  = 'Success';
