68909788e4264f0670851d8ccaf4e5786e9e4c09
root <root@localhost.localdomain>
2015-04-12 22:48:56 +0200

    MAIL FUNCT.

--- portal/model/account/customer.php
+++ portal/model/account/customer.php
@@ -75,6 +75,65 @@ class ModelAccountCustomer extends Model {
 		country_id = '" . (int)$data['country_id'] . "',
 		url = '" . $this->db->escape($data['url']) . "'");
 
+		$this->load->language('mail/customer');
+
+		$subject = sprintf($this->language->get('text_subject'), $this->config->get('config_name'));
+
+		$message = $this->language->get('text_greeting')."\n\n";
+		$message .= sprintf($this->language->get('text_welcome'), $this->config->get('config_name')) . "\n\n";
+
+		if (!$customer_group_info['approval']) {
+			$message .= $this->language->get('text_login') . "\n";
+		} else {
+			$message .= $this->language->get('text_approval') . "\n";
+		}
+
+		$message .= $this->url->link('account/login', '', 'SSL') . "\n\n";
+
+		$message .= sprintf($this->language->get('text_unique'),$unique_id) . "\n\n";
+		$message .= $this->language->get('text_services') . "\n\n";
+
+//		$message .= $this->language->get('text_thanks') . "\n\n";
+		$message .= $this->config->get('config_name');
+
+		$mail = new Mail($this->config->get('config_mail'));
+		$mail->setTo($data['email']);
+		$mail->setFrom($this->config->get('config_email'));
+		$mail->setSender($this->config->get('config_name'));
+		$mail->setSubject($subject);
+		$mail->setText(html_entity_decode($message, ENT_QUOTES, 'UTF-8'));
+		$mail->send();
+
+		// Send to main admin email if new account email is enabled
+		if ($this->config->get('config_account_mail')) {
+			$message  = $this->language->get('text_signup') . "\n\n";
+			$message .= $this->language->get('text_website') . ' ' . $this->config->get('config_name') . "\n";
+			$message .= $this->language->get('text_firstname') . ' ' . $data['firstname'] . "\n";
+			$message .= $this->language->get('text_lastname') . ' ' . $data['lastname'] . "\n";
+			$message .= $this->language->get('text_customer_group') . ' ' . $customer_group_info['name'] . "\n";
+			$message .= $this->language->get('text_email') . ' '  .  $data['email'] . "\n";
+			$message .= $this->language->get('text_telephone') . ' ' . $data['telephone'] . "\n";
+
+			$mail->setTo($this->config->get('config_email'));
+			$mail->setSubject(html_entity_decode($this->language->get('text_new_customer'), ENT_QUOTES, 'UTF-8'));
+			$mail->setText(html_entity_decode($message, ENT_QUOTES, 'UTF-8'));
+			$mail->send();
+
+			// Send to additional alert emails if new account email is enabled
+			$emails = explode(',', $this->config->get('config_mail_alert'));
+
+			foreach ($emails as $email) {
+				if (utf8_strlen($email) > 0 && preg_match('/^[^\@]+@.*\.[a-z]{2,6}$/i', $email)) {
+					$mail->setTo($email);
+					$mail->send();
+				}
+			}
+		}
+
+		$this->event->trigger('post.customer.add', $unique_id);
+
+		return $customer_id;
+
 	}
 
 	public function getCustomer($customer_id) {
