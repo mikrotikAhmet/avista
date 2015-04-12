5af70b42d99b1f3353223ef61a223a0e4551f770
root <root@localhost.localdomain>
2015-04-12 20:39:05 +0200

    Mail Templates

---
+++ portal/language/english/mail/customer.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *
+ * Semite ADP (Application Development Program) for PHP 5.1.6 or newer
+ *
+ * @package		Open Gateway Core Processor Application
+ * @author		Semite LLC. Dev Team
+ * @copyright	Copyright (c) 2008 - 2015, Semite LLC.
+ * @license		http://www.semitepayment.com/user_guide/license.html
+ * @link		http://www.semitepayment.com
+ * @version		Version 1.0.1
+ */
+// ------------------------------------------------------------------------
+
+
+
+// Text
+$_['text_subject']        = '%s - Thank you for registering';
+$_['text_greeting'] = 'Dear Client,';
+$_['text_welcome']        = 'Welcome to %s';
+$_['text_login']          = 'Your account has now been created and you can log in by using your email address and password by visiting our website or by clicking the following URL:';
+$_['text_approval']       = 'Your account must be approved before you can login. Once approved you can log in by using your email address and password by visiting our website or at the following URL:';
+$_['text_services']       = 'Upon logging in, you will be able to access all our Web Platform Services.';
+$_['text_disposal']         = 'We remain at your disposal in any further question or clarification.';
+$_['text_unique']         = 'Your Globally Unique Customer number is %s.';
+$_['text_thanks']         = 'Sincerely,';
+$_['text_new_customer']   = 'New customer';
+$_['text_signup']         = 'A new customer has signed up:';
+$_['text_website']        = 'Web Site:';
+$_['text_customer_group'] = 'Customer Group:';
+$_['text_firstname']      = 'First Name:';
+$_['text_lastname']       = 'Last Name:';
+$_['text_email']          = 'E-Mail:';
+$_['text_telephone']      = 'Telephone:';
+//End of file customer.php 
